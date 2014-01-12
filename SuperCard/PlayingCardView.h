cba4123953a7d9dcb7387c4f0baadcec91819930
Malachi Hul <malachihul@Malachis-MacBook-Pro.local>
2014-01-12 14:18:34 -0800

    first commit

---
+++ SuperCard/PlayingCardView.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardView.h
+//  SuperCard
+//
+//  Created by Malachi Hul on 1/7/14.
+//  Copyright (c) 2014 FanaticDesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayingCardView : UIView
+
+@end
---
+++ SuperCard/PlayingCardView.m
@@ -0,0 +1,31 @@
+//
+//  PlayingCardView.m
+//  SuperCard
+//
+//  Created by Malachi Hul on 1/7/14.
+//  Copyright (c) 2014 FanaticDesigns. All rights reserved.
+//
+
+#import "PlayingCardView.h"
+
+@implementation PlayingCardView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
